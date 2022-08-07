120d801ebbe31b77ec160fc5f201599626941caa
ducarett <marc.os.mercadocode@gmail.com>
2022-08-07 13:55:24 -0300

    cross origin provincias

--- src/main/java/com/javatechie/crud/example/controller/ProvinciaController.java
+++ src/main/java/com/javatechie/crud/example/controller/ProvinciaController.java
@@ -8,9 +8,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping("/provincia")
+@CrossOrigin(allowCredentials = "true", origins = "*", allowedHeaders = "*")
 public class ProvinciaController {
 
     @Autowired
